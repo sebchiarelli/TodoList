64c01e9105788ab044c5800838d1e6c5ad9f3872
sebchiarelli <47152370+sebchiarelli@users.noreply.github.com>
2020-05-23 19:29:36 +0200

    Add files via upload

--- Js/TodoList.js
+++ Js/TodoList.js
@@ -1,4 +1,22 @@
 //tire un trait sur un todo
-$("li").click(function(){
-    $(this).css("color", "red");
+$("ul").on("click", "li", function(){
+    $(this).toggleClass("completed");
+});
+
+$("ul").on("click", "span", function(){
+    $(this).parent().fadeOut(500,function(){
+        $(this).remove();
+    });
+    event.stopPropagation;
+});
+
+$("input[type='text']").keypress(function(event){
+    if(event.which === 13){
+        var todoText = $(this).val();
+        $(this).val("");
+        $("ul").append("<li><span><i class='fas fa-trash'></i></span> " + todoText + "</li>")
+    }  
+});
+$("#plus").click(function(){
+    $("input[type='text']").fadeToggle();
 });
